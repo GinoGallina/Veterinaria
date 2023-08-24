855db910402ac0695c43cab76a3c438713a80849
MartinSola55 <martinrsola55@gmail.com>
2023-08-24 14:44:26 -0300

    Layout format

--- vet/src/main/java/com/veterinaria/vet/Controllers/HomeController.java
+++ vet/src/main/java/com/veterinaria/vet/Controllers/HomeController.java
@@ -1,9 +1,11 @@
 package com.veterinaria.vet.Controllers;
 
+import org.springframework.security.config.web.server.ServerHttpSecurity.HttpsRedirectSpec;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -29,16 +31,18 @@ public class HomeController {
         return "Auth/Login"; // Nombre de la vista Thymeleaf (index.html)
     }
     @GetMapping("/inicio")
-    public String inicio() {
-        return "Home/Index"; // Nombre de la vista Thymeleaf (index.html)
+    public ModelAndView inicio(HttpSession session) {
+        ModelAndView modelAndView = new ModelAndView("Home/Index");
+        modelAndView.addObject("user_email", session.getAttribute("user_email"));
+        return modelAndView; // Nombre de la vista Thymeleaf (index.html)
     }
     @GetMapping("/prueba")
     public String prueba() {
         return "Shared/prueba"; // Nombre de la vista Thymeleaf (index.html)
     }
 
-    @RequestMapping("/**")
-        public void unmappedRequest() throws HttpRequestMethodNotSupportedException {
-            throw new HttpRequestMethodNotSupportedException(null);
-    }
+    // @RequestMapping("/**")
+    //     public void unmappedRequest() throws HttpRequestMethodNotSupportedException {
+    //         throw new HttpRequestMethodNotSupportedException(null);
+    // }
 }
